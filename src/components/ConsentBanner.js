6aa96652ab13e278ce7b8fef06427aba087ac64c
eacastel <emilio@aliigo.com>
2024-01-03 08:50:03 -0800

    008

--- src/components/ConsentBanner.js
+++ src/components/ConsentBanner.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from "@reach/router"; 
+import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies';
 
 const ConsentBanner = () => {
   const [showBanner, setShowBanner] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const consent = localStorage.getItem('gdprConsent');
@@ -10,12 +13,12 @@ const ConsentBanner = () => {
     }
   }, []);
 
-  const handleConsent = (consent) => {
-    localStorage.setItem('gdprConsent', consent);
+  const handleConsent = (consentValue) => {
+    localStorage.setItem('gdprConsent', consentValue);
     setShowBanner(false);
 
-    if (consent === 'true') {
-      // Push an event to the data layer
+    if (consentValue === 'true') {
+      initializeAndTrack(location);
       window.dataLayer = window.dataLayer || [];
       window.dataLayer.push({ 'event': 'allow-google-analytics' });
     }
@@ -26,14 +29,14 @@ const ConsentBanner = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-gray-600 bg-opacity-90 py-2 px-6 shadow-md text-center z-50 w-full">
       <p className="text-m mb-4 text-white font-bold">Este sitio web utiliza cookies.<br />
-      En nuestra web anonimizamos tus datos y solo utilizamos cookies para proporcionar un mejor servicio.</p>
+      En nuestra web anonimizamos tus datos personales y solo utilizamos cookies anónimas para proporcionar un mejor servicio.</p>
       <button 
         onClick={() => handleConsent('true')} 
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Aceptar
       </button>
       <button 
-        onClick={() => handleConsent('false')} 
+        onClick={() => handleConsent('false')}
         className="ml-4 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
         Rechazar
       </button>
