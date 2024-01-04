a408422092c202264230ed4054a67a27913d10be
eacastel <emilio@aliigo.com>
2024-01-04 02:10:07 -0800

    011

--- src/components/ConsentBanner.js
+++ src/components/ConsentBanner.js
@@ -1,45 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from "@reach/router"; 
+import React, { useEffect, useState } from 'react';
+import { useLocation } from '@reach/router';
 import { initializeAndTrack } from 'gatsby-plugin-gdpr-cookies';
 
-const ConsentBanner = () => {
-  const [showBanner, setShowBanner] = useState(false);
-  const location = useLocation();
+function isBrowser() {
+  return typeof window !== 'undefined';
+}
+
+function getValue(key, defaultValue) {
+  return isBrowser() && window.localStorage.getItem(key)
+    ? JSON.parse(window.localStorage.getItem(key))
+    : defaultValue;
+}
+
+function setValue(key, value) {
+  if (isBrowser()) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }
+}
+
+function useStickyState(defaultValue, key) {
+  const [value, setValueState] = useState(() => {
+    return getValue(key, defaultValue);
+  });
 
   useEffect(() => {
-    const consent = localStorage.getItem('gdprConsent');
-    if (consent === null) {
-      setShowBanner(true);
-    }
-  }, []);
+    setValue(key, value);
+  }, [key, value]);
 
-  const handleConsent = (consentValue) => {
-    localStorage.setItem('gdprConsent', consentValue);
-    setShowBanner(false);
+  return [value, setValueState];
+}
 
-    if (consentValue === 'true') {
-      // Initialize tracking after consent
+const ConsentBanner = () => {
+  const location = useLocation();
+  const [bannerHidden, setBannerHidden] = useStickyState(false, 'gdprConsent');
+
+  const enableAnalytics = () => {
+    setValue('gdprConsent', 'true');
+    setBannerHidden(true);
+    if (isBrowser()) {
       initializeAndTrack(location);
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({ 'event': 'allow-google-analytics' });
     }
   };
 
-  if (!showBanner) return null;
+  const rejectConsent = () => {
+    setBannerHidden(true);
+  };
+
+  if (bannerHidden) return null;
 
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-gray-600 bg-opacity-90 py-2 px-6 shadow-md text-center z-50 w-full">
       <p className="text-m mb-4 text-white font-bold">Este sitio web utiliza cookies.<br />
       En nuestra web anonimizamos tus datos personales y solo utilizamos cookies anónimas para proporcionar un mejor servicio.</p>
       <button 
-        onClick={() => handleConsent('true')} 
+        onClick={enableAnalytics}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Aceptar
       </button>
       <button 
-        onClick={() => handleConsent('false')}
+        onClick={rejectConsent}
         className="ml-4 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
         Rechazar
       </button>
-    </div>
+      </div>
   );
 };
 
